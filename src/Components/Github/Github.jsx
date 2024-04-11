89b1ab0b81765c83c47c1c3ba477c3ef8e72eb7f
Abhishek-Bhandare <abhishekbhandare1@gmail.com>
2024-04-11 19:06:16 +0530

    first commit

---
+++ src/Components/Github/Github.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+
+function Github() {
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch('https://api.github.com/users/Abhishek-Bhandare')
+    .then(response => response.json())
+    .then((data) => {
+      // console.log(data);
+       setData(data)
+    })
+  }, [])
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white text-3xl p-4'>
+      Github Followers : {data.followers}
+      <img src={data.avatar_url} alt='Git Picture' width={300}/>
+    </div>
+  )
+}
+
+export default Github
--- src/index.js
+++ src/index.js
@@ -2,16 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import User from './Components/User/User';
+import Github from './Components/Github/Github';
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element:<App/>,
+//     children: [
+//       {
+//         path:"",
+//         element:<Home/>
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       },
+//       {
+//         path:"contactus",
+//         element:<Contact/>
+//       }
+//     ]
+//   }
+// ])
+
+// 2nd method
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+    <Route path='' element={<Home/>}/>
+    <Route path='about' element={<About/>}/>
+    <Route path='contactus' element={<Contact/>}/>
+    <Route path='user/:id' element={<User/>}/>
+    <Route path='github' element={<Github/>}/>
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
